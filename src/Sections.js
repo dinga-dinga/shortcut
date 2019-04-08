ad1c6260d15a8277a3cdf12adaac84ff74a2f9c8
dingadinga <yotamamir96@gmail.com>
2019-04-08 18:51:10 +0300

    added Sections

---
+++ src/Sections.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Sections.css'
+class Sections extends React.Component {
+
+  render() {
+    return (
+      <div class="center-div">
+        <ul id="menu">
+          <li>Mador 1</li>
+          <li>Mador 2</li>
+          <li>Mador 12</li>
+          <li>Mador 14</li>
+          <li>Mador 15</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default Sections;
